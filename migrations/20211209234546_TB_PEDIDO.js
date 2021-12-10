5be91b67fd9d3343fa0a0fdc7660119e50c2d594
john9900k <jhonny.cunha.simoes63@aluno.ifce.edu.br>
2021-12-10 00:47:25 -0300

    atualizando arquivos

--- migrations/20211209234546_TB_PEDIDO.js
+++ migrations/20211209234546_TB_PEDIDO.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('TB_PRODUTO', table => {
+        table.increments('id').primary()
+        table.string('data_pedido').notNull()
+        table.integer('cliente_id').unsigned()
+        table.foreign('cliente_id').references('TB_CLIENTE.id')
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('TB_PEDIDO')
 };
