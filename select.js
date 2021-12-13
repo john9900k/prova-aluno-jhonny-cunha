08f7c541d348368d86d0c3ec01aa0253ef8cbed9
john9900k <jhonny.cunha.simoes63@aluno.ifce.edu.br>
2021-12-13 13:15:00 -0300

    create select.js

---
+++ select.js
@@ -0,0 +1,22 @@
+const knex = require('knex') ({
+    client: 'sqlite3',
+    connection: {
+      filename: './banco/dev.sqlite3'
+    }
+})
+
+knex.select().table('TB_CLIENTE').then(data => {
+    console.log(data)
+}).catch(error => console.log(error))
+
+knex.select().table('TB_PRODUTO').then(data => {
+    console.log(data)
+}).catch(error => console.log(error))
+
+knex.select().table('TB_PEDIDO').then(data => {
+    console.log(data)
+}).catch(error => console.log(error))
+
+knex.select().table('TB_PEDIDO_PRODUTO').then(data => {
+    console.log(data)
+}).catch(error => console.log(error))
